a55d258ab94ac90af047c0edb3ffab17b913f6f2
Blake Sweeney <blakes.85@gmail.com>
2015-05-16 19:50:25 -0400

    A simple setup for the viewer
    The idea here is to create something which can superimpose selected loops. This
    will probably get expanded to be nucleotide collections. This will be useful to
    the lab (hopefully) and be a good test for the superimposing functionality of
    pv.

---
+++ static/js/app.js
@@ -0,0 +1,13 @@
+(document.ready(function() {
+  'use strict';
+
+  var viewer = pv.Viewer(document.getElementById('viewer'), {
+    'width': 'auto',
+    'height': 'auto',
+    antialias: true,
+    quality: 'medium'
+  });
+
+  $('#display-btn').click(function() {
+  });
+});
